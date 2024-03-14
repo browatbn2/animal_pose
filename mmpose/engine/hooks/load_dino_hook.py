47cfb7b87e704d914d4c29f3a9d593c9b6b59c1f
Bjoern Browatzki <bjoern@eye2you.ai>
2024-03-14 01:23:28 +0100

    add ablation configs

--- mmpose/engine/hooks/load_dino_hook.py
+++ mmpose/engine/hooks/load_dino_hook.py
@@ -16,10 +16,13 @@ from mmpose.registry import HOOKS
 DATA_BATCH = Optional[Union[dict, tuple, list]]
 
 
-def get_dinov2_filepath(dataset, split, arch, patch_size):
+def get_dinov2_filepath(dataset, split, arch, patch_size, lowres=False):
     username = os.environ.get('USER')
     out_dir = f'/home/{username}/dev/data/dino'
-    return os.path.join(out_dir, f'dino_{dataset}_{split}_{arch}_{patch_size}.h5')
+    if lowres:
+        return os.path.join(out_dir, f'dino_{dataset}_{split}_{arch}_{patch_size}.lowres.h5')
+    else:
+        return os.path.join(out_dir, f'dino_{dataset}_{split}_{arch}_{patch_size}.h5')
 
 
 def load_dino_hdf5(filepath: str) -> h5py.File:
@@ -35,12 +38,14 @@ class LoadDinoHook(Hook):
                  train_split='train-split1',
                  val_split='val-split1',
                  arch='vits14',
-                 patch_size=14):
+                 patch_size=14,
+                 lowres=False):
 
         self.dino_hdf5 = None
         self.dino_features = {}
+        self.lowres = lowres
 
-        dino_file = get_dinov2_filepath(dataset, split=train_split, arch=arch, patch_size=patch_size)
+        dino_file = get_dinov2_filepath(dataset, split=train_split, arch=arch, patch_size=patch_size, lowres=lowres)
         if dino_file is not None:
             assert os.path.isfile(dino_file), f"Could not find DINO feature file {dino_file}"
             self.dino_hdf5_train = load_dino_hdf5(dino_file)
@@ -71,8 +76,13 @@ class LoadDinoHook(Hook):
             # data_sample.set_data({'dino': feat})
 
         attentions = torch.tensor(np.array(features), device='cuda')
+        if self.lowres:
+            # resize to 'highres' resolution (64x64)
+            attentions = kornia.geometry.resize(attentions, (64, 64))
+
         dino_warp_mats = torch.tensor(np.array([s.dino_warp_mat for s in data['data_samples']]), device='cuda')
         attentions = kornia.geometry.affine(attentions, dino_warp_mats)
+
         for i, data_sample in enumerate(data['data_samples']):
             data_sample.set_data({'dino': attentions[i]})
 
