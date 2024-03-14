c833ddec301bee11cad952b359e8a4f4a460032a
Bjoern Browatzki <bjoern@eye2you.ai>
2024-03-14 01:49:08 +0100

    add ablation configs

---
+++ configs/animal_2d_keypoint/dinopose/ablation/noaugs_distill_hrnet_ap10k-256x256.py
@@ -0,0 +1,194 @@
+_base_ = ['../../../_base_/default_runtime.py']
+
+# runtime
+train_cfg = dict(max_epochs=100, val_interval=5)
+
+# optimizer
+optim_wrapper = dict(optimizer=dict(
+    type='Adam',
+    lr=1e-4,
+))
+
+# learning policy
+# param_scheduler = [
+#     dict(
+#         type='LinearLR', begin=0, end=500, start_factor=0.001,
+#         by_epoch=False),  # warm-up
+#     dict(
+#         type='MultiStepLR',
+#         begin=0,
+#         end=210,
+#         milestones=[30, 60],
+#         gamma=0.1,
+#         by_epoch=True)
+# ]
+
+# automatically scaling LR based on the actual training batch size
+auto_scale_lr = dict(base_batch_size=512)
+
+# hooks
+default_hooks = dict(
+    logger=dict(type='LoggerHook', interval=40),
+    checkpoint=dict(type='CheckpointHook', interval=5),
+)
+
+default_hooks.update(dict(load_dino=dict(type='LoadDinoHook',
+                                         dataset='ap10k',
+                                         train_split='train-split1',
+                                         val_split='val-split1',
+                                         arch='vitl14',
+                                         patch_size=14,
+                                         )))
+
+
+# codec settings
+codec = dict(type='MSRAHeatmap', input_size=(256, 256), heatmap_size=(64, 64), sigma=2)
+
+embedding_dim = 128
+dino_dim = 1024
+
+# model settings
+model = dict(
+    type='DinoPoseEstimator',
+    distill=True,
+    data_preprocessor=dict(
+        type='PoseDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True),
+    backbone=dict(
+        type='HRNet',
+        in_channels=3,
+        extra=dict(
+            stage1=dict(
+                num_modules=1,
+                num_branches=1,
+                block='BOTTLENECK',
+                num_blocks=(4,),
+                num_channels=(64,)),
+            stage2=dict(
+                num_modules=1,
+                num_branches=2,
+                block='BASIC',
+                num_blocks=(4, 4),
+                num_channels=(32, 64)),
+            stage3=dict(
+                num_modules=4,
+                num_branches=3,
+                block='BASIC',
+                num_blocks=(4, 4, 4),
+                num_channels=(32, 64, 128)),
+            stage4=dict(
+                num_modules=3,
+                num_branches=4,
+                block='BASIC',
+                num_blocks=(4, 4, 4, 4),
+                num_channels=(32, 64, 128, 256))),
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='https://download.openmmlab.com/mmpose/pretrain_models/hrnet_w32-36af842e.pth'),
+    ),
+    neck=dict(
+        type='HeatmapHead',
+        in_channels=32,
+        out_channels=embedding_dim,
+        deconv_out_channels=None,
+    ),
+    head=dict(
+        type='HeatmapHead',
+        in_channels=embedding_dim,
+        out_channels=17,
+        deconv_out_channels=None,
+        loss=dict(type='KeypointMSELoss', use_target_weight=True),
+        decoder=codec),
+    dino_decoder=dict(
+        type='HeatmapHead',
+        in_channels=embedding_dim,
+        out_channels=dino_dim,
+        deconv_out_channels=None,
+    ),
+    test_cfg=dict(
+        flip_test=True,
+        flip_mode='heatmap',
+        shift_heatmap=True,
+    ),
+)
+
+# base dataset settings
+dataset_type = 'AP10KDataset'
+data_mode = 'topdown'
+data_root = '/home/browatbn/dev/datasets/animal_data/ap-10k/'
+
+# pixel_augmentations = dict(
+#     type='Albumentation',
+#     transforms=[
+#         dict(type='CoarseDropout',
+#              min_width=8, min_height=8,
+#              max_width=64, max_height=64,
+#              min_holes=1,
+#              max_holes=1,
+#              p=1.00),
+#         dict(type='RandomBrightnessContrast', brightness_limit=0.3, contrast_limit=0.3),
+#         dict(type='HueSaturationValue', hue_shift_limit=20*2, sat_shift_limit=30*2, val_shift_limit=20*2),
+#         dict(type='RGBShift'),
+#         dict(type='RandomGamma'),
+#         dict(type='Blur', blur_limit=11, p=0.1),
+#         dict(type='MotionBlur', blur_limit=11, p=0.1),
+#         dict(type='GaussNoise', p=0.1),
+#         dict(type='CLAHE', p=0.1),
+#     ])
+
+# pipelines
+train_pipeline = [
+    dict(type='LoadImage'),
+    dict(type='GetBBoxCenterScale'),
+    # dict(type='RandomFlip', direction='horizontal'),
+    # dict(type='RandomBBoxTransform', rotate_factor=40, shift_prob=0),
+    dict(type='TopdownAffine', input_size=codec['input_size']),
+    dict(type='TopdownAffineDino', input_size=codec['input_size'], input_size_dino=codec['heatmap_size']),
+    dict(type='GenerateTarget', encoder=codec),
+]
+
+
+# train_pipeline.append(pixel_augmentations)
+
+train_pipeline.append(
+    dict(type='PackPoseInputs',
+         meta_keys=('id', 'img_id', 'img_path', 'category_id', 'crowd_index', 'ori_shape', 'img_shape',
+        'input_size', 'input_center', 'input_scale', 'flip', 'flip_direction', 'flip_indices',
+        'raw_ann_info', 'dataset_name', 'dino_warp_mat', 'mask'),
+         pack_transformed=True)
+)
+
+val_pipeline = [
+    dict(type='LoadImage'),
+    dict(type='GetBBoxCenterScale'),
+    dict(type='TopdownAffine', input_size=codec['input_size']),
+    dict(type='TopdownAffineDino', input_size=codec['input_size'], input_size_dino=codec['heatmap_size']),
+    dict(type='GenerateTarget', encoder=codec),
+    dict(type='PackPoseInputs',
+         meta_keys=('id', 'img_id', 'img_path', 'category_id', 'crowd_index', 'ori_shape', 'img_shape',
+                    'input_size', 'input_center', 'input_scale', 'flip', 'flip_direction', 'flip_indices',
+                    'raw_ann_info', 'dataset_name', 'dino_warp_mat', 'mask'),
+         pack_transformed=True)
+]
+
+indices_train = None
+
+# data loaders
+train_dataloader = dict(
+    batch_size=16,
+    num_workers=4,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_mode=data_mode,
+        ann_file='annotations/ap10k-train-split1.json',
+        data_prefix=dict(img='data/'),
+        pipeline=train_pipeline,
+        indices=indices_train,
+    ))
+val_cfg = None
+test_cfg = None
